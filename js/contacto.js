563e10ee63d8b7233a2d91db5d47efaa18f11f80
Yessica De Los Santos <dyessica919@gmail.com>
2026-06-08 21:48:28 -0600

    Se soluciona error de alerta

--- js/contacto.js
+++ js/contacto.js
@@ -1,4 +1,15 @@
-const enviar = () => {
+document.addEventListener("DOMContentLoaded", () => {
+    const btnContact = document.getElementById("btnContact");
+    btnContact.addEventListener("click", (e) => {
+        e.preventDefault();
+        enviar();
+    });
+
+
+});
+
+
+function enviar() {
     const nameContact = document.getElementById("nameContact");
     const lastNameContact = document.getElementById("lastNameContact");
     const emailContact = document.getElementById("emailContact");
